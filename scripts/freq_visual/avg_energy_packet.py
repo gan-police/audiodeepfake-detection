577c284948e68a69aed20fd22fcdde1d1bf84495
moritz@wolter.tech <moritz@wolter.tech>
2023-04-19 11:55:47 +0200

    packet plot code.

--- scripts/freq_visual/avg_energy_packet.py
+++ scripts/freq_visual/avg_energy_packet.py
@@ -31,10 +31,10 @@ def compute_pytorch_packet_representation(
     ptwt_wp_tree = ptwt.WaveletPacket(data=pt_data, wavelet=wavelet, mode="reflect")
 
     # get the pytorch decomposition
-    wp_keys = list(product(["a", "d"], repeat=max_lev))
+    wp_keys = ptwt_wp_tree.get_level(max_lev)
     packet_list = []
     for node in wp_keys:
-        packet_list.append(torch.squeeze(ptwt_wp_tree["".join(node)], 0))
+        packet_list.append(torch.squeeze(ptwt_wp_tree[node], 0))
 
     wp_pt = torch.stack(packet_list, dim=-1)
     return wp_pt
@@ -44,14 +44,29 @@ def compute_pytorch_packet_representation(
 if __name__ == "__main__":    
 
     dataset = LearnWavefakeDataset(data_dir='/home/wolter/uni/audiofake/data/ljspeech_22050_11025_0.7_train')
+    mean_dict = {}
     for it, batch in enumerate(
         tqdm(
             iter(dataset),
             desc="Training",
+            total=len(dataset)
         )
     ):
         batch_audios = batch['audio'].cuda(non_blocking=True)
         packets = compute_pytorch_packet_representation(batch_audios)
-        plt.imshow(torch.log(torch.abs(packets.cpu().T)))
-        plt.show()
-        pass
+        # plt.imshow(torch.log(torch.abs(packets.cpu())).T)
+        # plt.show()
+        mean_packets = torch.mean(torch.log(torch.abs(packets)), 0)
+        label = int(batch['label'])
+        if label in mean_dict.keys():
+            mean_dict[label].append(mean_packets)
+        else:
+            mean_dict[label] = [mean_packets]
+    
+    for key in mean_dict.keys():
+        plt.plot(torch.mean(torch.stack(mean_dict[key], 0), 0).cpu().numpy(),
+                 label=key)
+    plt.legend()
+    plt.show()
+    
+    pass
